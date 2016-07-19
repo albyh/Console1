8403fc0e661a1dc9d9e859db4c376e2c82937f95
albyh <albyheredia@gmail.com>
2016-07-19 11:00:11 -0700

    Add project files.

---
+++ Console1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt user to enter a name.
+            Console.WriteLine("Enter your name, please:");
+            // Now read the name entered
+            string name = Console.ReadLine();
+            // Greet the user with the entered name
+            Console.WriteLine("Hello, " + name);
+            // Wait for the user to acknowledge
+            Console.WriteLine("Press Enter to quit...");
+            Console.Read();
+        }
+    }
+}
